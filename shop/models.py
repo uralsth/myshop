c5dbd096c353aa340830c639e0481cc92662d8e2
uralsth <Shresthaural100@gmail.com>
2022-05-30 16:31:55 +0545

    added creator section

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your models here.
@@ -25,6 +26,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
+    creator = models.ForeignKey(User, related_name='shop_products', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)

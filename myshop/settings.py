c6beabf9b3eaf28d5d8dce291a8a815c47a37186
uralsth <Shresthaural100@gmail.com>
2022-05-31 19:47:42 +0545

    added cart app and cart session id

--- myshop/settings.py
+++ myshop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,6 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_URL = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = config('CART_SESSION_ID')
+

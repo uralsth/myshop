13803325bda1e1d446bb22fab0f22f0822d189d6
uralsth <Shresthaural100@gmail.com>
2022-06-02 21:10:39 +0545

    added update and remove feature

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 # Create your views here.
 
@@ -14,4 +15,5 @@ def product_list(request, category_slug=None):
 
 def product_detail (request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product': product})
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})

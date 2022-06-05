a99081da1866cbff8a50f655ec16d4162901741c
uralsth <Shresthaural100@gmail.com>
2022-06-05 18:36:37 +0545

    included orders urlpatterns in project url file

--- myshop/urls.py
+++ myshop/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='order_create')),
     path('', include('shop.urls', namespace='shop')),
 ]
 if settings.DEBUG:

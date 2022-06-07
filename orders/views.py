d92d8d645db4e031de2bc5e99481deb01b14fc36
uralsth <Shresthaural100@gmail.com>
2022-06-07 11:12:17 +0545

    integrated payment links

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
+from django.urls import reverse
+
 
 # Create your views here.
 def order_create(request):
@@ -17,7 +19,11 @@ def order_create(request):
             cart.clear()
             # launch asynchronous task
             order_created.delay(order.id)
-            return render(request, 'orders/order/created.html', {'order': order})
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect for payment
+            return redirect(reverse('payment:process'))
+            # return render(request, 'orders/order/created.html', {'order': order})
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})

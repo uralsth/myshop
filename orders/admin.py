6c61681ba8b49c4d8aa330ea42fc4c4bc47e3823
uralsth <Shresthaural100@gmail.com>
2022-06-07 19:43:31 +0545

    custom admin action (export to csv)

--- orders/admin.py
+++ orders/admin.py
@@ -6,6 +6,31 @@ class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
 
+import csv
+import datetime
+from django.http import HttpResponse
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    contet_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = contet_disposition
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):

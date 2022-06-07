2a91bccba5b52f5bc713e2a00ac9087235c9e52f
uralsth <Shresthaural100@gmail.com>
2022-06-07 19:45:21 +0545

    added a custom view to display information about order

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
-from .models import OrderItem
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 # Create your views here.
@@ -27,3 +28,8 @@ def order_create(request):
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})

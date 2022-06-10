0d9859e4370dc7a3c37c459f2b941d6eee18cda3
uralsth <Shresthaural100@gmail.com>
2022-06-10 20:38:07 +0545

    added location for static root directory

--- myshop/settings.py
+++ myshop/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-from django.conf.global_settings import EMAIL_BACKEND, MEDIA_URL
+from django.conf.global_settings import EMAIL_BACKEND, MEDIA_URL, STATIC_ROOT
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_URL = os.path.join(BASE_DIR, 'media/')
@@ -148,3 +149,4 @@ BRAINTREE_CONF = braintree.Configuration(
     BRAINTREE_PUBLIC_KEY,
     BRAINTREE_PRIVATE_KEY
 )
+

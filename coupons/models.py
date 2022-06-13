e6e44243bed9eee928b07043d4eb81d8f330fbcf
uralsth <Shresthaural100@gmail.com>
2022-06-13 10:26:15 +0545

    fixed syntax error

--- coupons/models.py
+++ coupons/models.py
@@ -4,9 +4,9 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Coupon(models.Model):
     code = models.CharField(max_length=50, unique=True)
-    valid_form = models.DateTimeField()
+    valid_from = models.DateTimeField()
     valid_to = models.DateTimeField()
-    discount = models.IntegerField(validators=(MinValueValidator(0), MaxValueValidator(10)))
+    discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     active = models.BooleanField()
 
     def __str__(self):

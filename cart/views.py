fb422aa175eaf088654155b1696c07f29d77977f
uralsth <Shresthaural100@gmail.com>
2022-06-17 16:15:13 +0545

    rendered recommender

--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,7 @@ from coupons.forms import CouponApplyForm
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+from shop.recommender import Recommender
 # Create your views here.
 
 @require_POST
@@ -29,4 +30,7 @@ def cart_detail(request):
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'override': True})
     coupon_apply_form = CouponApplyForm()
-    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    recommended_products = r.suggest_products_for(cart_products, max_results=4)
+    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form, 'recommended_products': recommended_products})
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 from cart.forms import CartAddProductForm
+from .recommender import Recommender
 
 # Create your views here.
 
@@ -16,4 +17,6 @@ def product_list(request, category_slug=None):
 def product_detail (request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
-    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
+    r = Recommender()
+    recommended_products = r.suggest_products_for([product], 4)
+    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form, 'recommended_products': recommended_products})

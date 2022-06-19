0b6e522bf40501b7f3998a2b7bcbf3f212496cf3
uralsth <Shresthaural100@gmail.com>
2022-06-19 20:29:31 +0545

    inclded accounts urls

--- myshop/urls.py
+++ myshop/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='order_create')),

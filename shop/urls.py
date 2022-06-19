001f7bc31a667a3d50160776f6b10931fd7f890f
uralsth <Shresthaural100@gmail.com>
2022-06-19 20:30:11 +0545

    urls for dashboard

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]

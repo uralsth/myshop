810348c8709488eb9695b1ef7da288480855a1f6
uralsth <shresthaural100@gmail.com>
2022-06-30 21:08:16 +0545

    included urlpatterns for editting

--- account/urls.py
+++ account/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]

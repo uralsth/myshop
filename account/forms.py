6b99d8dc35aa32c2437026318c37157010046733
uralsth <shresthaural100@gmail.com>
2022-06-30 21:07:10 +0545

    created edit form models

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -24,3 +26,15 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Password don\'t match.')
         return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'address', 'photo']

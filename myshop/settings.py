fa3ef059845825fcfc586397ec3904c187540142
uralsth <shresthaural100@gmail.com>
2022-07-11 20:51:04 +0545

    added allowed host and social-auth extension

--- myshop/settings.py
+++ myshop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(' ') 
 
 
 # Application definition
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'orders',
     'payment',
     'coupons',
+
+    'social_django',
 ]
 
 MIDDLEWARE = [
